660e7a29a6e978b6a28cb9669cafed80e77cbf0c
Daniel Ruderman <rudermandaniel@gmail.com>
2021-05-24 00:50:18 -0700

    Adding rest of project I already made progress on locally

---
+++ controllers.py
@@ -0,0 +1,111 @@
+"""
+This file defines actions, i.e. functions the URLs are mapped into
+The @action(path) decorator exposed the function at URL:
+
+    http://127.0.0.1:8000/{app_name}/{path}
+
+If app_name == '_default' then simply
+
+    http://127.0.0.1:8000/{path}
+
+If path == 'index' it can be omitted:
+
+    http://127.0.0.1:8000/
+
+The path follows the bottlepy syntax.
+
+@action.uses('generic.html')  indicates that the action uses the generic.html template
+@action.uses(session)         indicates that the action uses the session
+@action.uses(db)              indicates that the action uses the db
+@action.uses(T)               indicates that the action uses the i18n & pluralization
+@action.uses(auth.user)       indicates that the action requires a logged in user
+@action.uses(auth)            indicates that the action requires the auth object
+
+session, db, T, auth, and tempates are examples of Fixtures.
+Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
+"""
+
+from py4web import action, request, abort, redirect, URL
+from yatl.helpers import A
+from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from py4web.utils.url_signer import URLSigner
+from .models import get_user_email
+
+url_signer = URLSigner(session)
+
+#@action('index')
+#@action.uses(db, auth, 'index.html')
+#def index():
+#    return dict(
+#        # COMPLETE: return here any signed URLs you need.
+#        my_callback_url = URL('my_callback', signer=url_signer),
+#    )
+
+@action('index')
+@action.uses(db, auth, 'index.html')
+def index():
+    rows = db(db.brawl.num_of_public != 0).select(db.brawl.ALL, orderby=~db.brawl.num_of_public, limitby=(0, 10)).as_list()
+    counter = 0
+    for row in rows:
+        counter = counter + 1
+        row["count"] = counter
+        items = db(db.item.brawl_id == row["id"]).select(db.item.ALL, orderby=~db.item.num_of_wins)
+        wins_list = []
+        name_list = []
+        for _item in items:
+            wins_list.append(_item.num_of_wins)
+            name_list.append((db.item_name[_item.item_name_id]).item_str)
+        row["_num_of_wins"] = wins_list
+        row["_item_name"] = name_list
+    return dict(rows=rows, search_url = URL('search', signer=url_signer))
+    
+@action('search')
+@action.uses()
+def search():
+    #qs = filter(None, re.split(';|"| \'|,|/|\\ |\s', q))
+    
+    rows = []
+    
+    q = request.params.get("q")
+    qs = q.split()
+    
+    _query = False
+    for word in qs:
+        _query = _query | (db.item_name.item_str == word)
+    item_names = db(_query).select()
+    
+    _query = False
+    for _item_name in item_names:
+        _query = _query | (db.item.item_name_id == _item_name.id)
+        print(db(db.item.item_name_id == _item_name.id).count())
+    
+    if _query:
+        items = db(_query).select()
+        
+        _query = False
+        for _item in items:
+            _query = _query | (db.brawl.id == _item.brawl_id)
+        _query = _query & (db.brawl.num_of_public != 0)
+
+        if _query:
+            rows = db(_query).select(db.brawl.ALL, orderby=~db.brawl.num_of_public, limitby=(0, 10)).as_list()
+            count = 0
+            for row in rows:
+                items = db(db.item.brawl_id == row["id"]).select(db.item.ALL, orderby=~db.item.num_of_wins)
+                wins_list = []
+                name_list = []
+                for _item in items:
+                    wins_list.append(_item.num_of_wins)
+                    name_list.append((db.item_name[_item.item_name_id]).item_str)
+                row["_num_of_wins"] = wins_list
+                row["_item_name"] = name_list
+        
+    return dict(results=rows)
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ models.py
@@ -0,0 +1,45 @@
+"""
+This file defines the database models
+"""
+
+import datetime
+from .common import db, Field, auth
+from pydal.validators import *
+
+
+def get_user_email():
+    return auth.current_user.get('email') if auth.current_user else None
+
+def get_time():
+    return datetime.datetime.utcnow()
+
+db.define_table(
+    'brawl',
+    Field('item_search_str', requires=IS_NOT_EMPTY()),
+    Field('num_of_public', 'integer', default=0, requires=IS_INT_IN_RANGE(0, 1e6)),
+    Field('num_of_plays', 'integer', default=1, requires=IS_INT_IN_RANGE(0, 1e6)),
+)
+
+db.define_table(
+    'item_name',
+    Field('item_str', requires=IS_NOT_EMPTY()),
+    Field('num_of_uses', 'integer', default=1, requires=IS_INT_IN_RANGE(0, 1e6)),
+)
+
+db.define_table(
+    'item',
+    Field('item_name_id', 'reference item_name'),
+    Field('num_of_wins', 'integer', default=0, requires=IS_INT_IN_RANGE(0, 1e6)),
+    Field('brawl_id', 'reference brawl'),
+)
+
+db.define_table(
+    'user_brawl',
+    Field('brawl_id', 'reference brawl'),
+    Field('placement_order', requires=IS_NOT_EMPTY()),
+    Field('created_by', default=get_user_email),
+    Field('creation_date', 'datetime', default=get_time),
+    Field('public', 'boolean', default=False),
+)
+
+db.commit()
